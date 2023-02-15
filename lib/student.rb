68efc3c9d7c24bb82f2213a7023e6e8e4b18aacd
Jeff Nelson <jpnelson85@gmail.com>
2023-02-15 10:33:01 -0600

    add final part of iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,20 @@
 class Student
   attr_reader :name,
-              :age
+              :age,
+              :scores
 
   def initialize(info)
     @name = info[:name]
     @age = info[:age]
+    @scores = []
+  end
 
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade(scores)
+    average = scores.sum / @scores.length
+    average
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe Student do
     student = Student.new({name: "Morgan", age: 21}) 
     expect(student.age).to eq(21)
   end
+
+  it 'scores attribute equals empty array' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    expect(student.scores).to eq([])
+  end
+
+  it 'add scores' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq([89, 78])
+  end
+
+  it 'average of grades' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.average([89, 78])).to eq(83.5)
+  end
 end
