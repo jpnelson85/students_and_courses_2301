432c6088823e4a70cc06f2f856ccac6ecea71455
Jeff Nelson <jpnelson85@gmail.com>
2023-02-15 10:11:54 -0600

    add tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,6 @@
 class Student
+  attr_reader :name,
+              :age
 
   def initialize(info)
     @name = info[:name]
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Student do
     student = Student.new({name: "Morgan", age: 21}) 
     expect(student).to be_a(Student)
   end
+
+  it 'student name Morgan' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    expect(student.name).to eq("Morgan")
+  end
+
+  it 'student age is 21' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    expect(student.age).to eq(21)
+  end
 end
