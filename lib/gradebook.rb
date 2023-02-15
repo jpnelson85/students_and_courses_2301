8549dbc2f70ca2b73849d98bb14657454bb69f00
Jeff Nelson <jpnelson85@gmail.com>
2023-02-15 11:28:17 -0600

    add to iteration 3

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,10 +1,23 @@
 class Gradebook
-  attr_reader :instructor
-              :courses
+  attr_reader   :instructor
+              
+  attr_accessor :courses
 
   def initialize(instructor)
     @instructor = instructor
     @courses = []
-    
   end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+
+  end
+
+  def students_below
+
+  end
+
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -1,9 +1,28 @@
 require 'rspec'
 require './lib/student'
 require './lib/course'
+require './lib/gradebook'
 
 RSpec.describe Gradebook do
   it 'exists' do
     gradebook = Gradebook.new("Mr. Nelson")
+    expect(gradebook).to be_a(Gradebook)
+  end
+
+  it 'has attributes instructor and courses' do
+    gradebook = Gradebook.new("Mr. Nelson")
+    expect(gradebook.instructor).to eq("Mr. Nelson")
+    expect(gradebook.courses).to eq([])
+  end
+
+  it 'add courses to @courses array' do
+    gradebook = Gradebook.new("Mr. Nelson")
+    course1 = Course.new("Calculus", 2)
+    gradebook.add_course(course1)
+    expect(gradebook.courses).to eq([course1])
+  end
+
+  it 'create hash with key as Course and value as array of Student objects' do
+    
   end
 end
